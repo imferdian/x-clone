4985deb971e076d6c738ec151f094ef5a3696561
imferdian <ferdiansyahtri30@gmail.com>
2025-01-28 21:09:45 +0800

    persiapan sebelum deployment

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', routes);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
